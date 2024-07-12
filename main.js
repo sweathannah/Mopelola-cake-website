1df58a75e05f4c6bb5fc0b1fbf0ac9098e11618f
sweathannah <sultanatmbashir@gmail.com>
2024-07-12 16:54:54 +0100

    Our Story in Motion

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     let menulink = document.getElementById("menu");
     let menuClose = document.getElementById("closeDropdown");
-    let dropdownmenu = document.getElementById('mobileMenu');
+    let dropdownmenu = document.getElementById("mobileMenu");
 
-    if (menulink && menuClose && dropdownmenu) {
-        menulink.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent default action
-            dropdownmenu.classList.remove("hidden")
-            menulink.classList.add("hidden");
-        });
+    menulink.addEventListener("click", function () {
+        dropdownmenu.classList.remove("hidden");
 
-        menuClose.addEventListener("click", function () {
-            dropdownmenu.classList.add("hidden");
-            menulink.classList.remove("hidden");
-        });
-    } else {
-        console.log("One or more elements not found");
-    }
+    });
+
+    menuClose.addEventListener("click", function () {
+        dropdownmenu.classList.add("hidden");
+    });
 });
+
+
